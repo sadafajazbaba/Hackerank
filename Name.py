e9030e47c6d702f64890b7291b94698b80574a05
Sadafajazbaba <babasadaf4@gmail.com>
2024-05-26 10:17:43 +0530

    Simple HackeRank problem

---
+++ Name.py
@@ -0,0 +1,13 @@
+
+# Complete the 'print_full_name' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING first
+#  2. STRING last
+#
+first_name = input()
+last_name = input()
+def print_full_name(first, last):
+    print(f"Hello {first_name} {last_name}! You just delved into python.")
+print_full_name(first_name, last_name)
